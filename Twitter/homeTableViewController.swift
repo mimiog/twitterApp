876e3625e528b81492931e93a3329ced20f69dca
mimiog <98125679+mimiog@users.noreply.github.com>
2022-03-19 00:24:06 -0400

    implemented phase 2

--- Twitter/homeTableViewController.swift
+++ Twitter/homeTableViewController.swift
@@ -24,6 +24,15 @@ class homeTableViewController: UITableViewController {
         tableView.refreshControl = myRefreshControl
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
+    
+    
+    
     @objc func loadTweets() {
         
         numberOfTweet = 20
@@ -42,6 +51,7 @@ class homeTableViewController: UITableViewController {
         }, failure: { (Error) in
             print("Could not retrieve tweets! Sorry")
         })
+        
     }
     
     func loadMoreTweets(){
@@ -64,17 +74,9 @@ class homeTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath:IndexPath) {
         if indexPath.row + 1 == tweetArray.count {
             loadMoreTweets()
+        
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-  
     
 
     @IBAction func onLogout(_ sender: Any) {
@@ -92,6 +94,7 @@ class homeTableViewController: UITableViewController {
         cell.userNameLabel.text = user["name"] as? String
         cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
         
+        
         let imageUrl = URL (string: (user["profile_image_url_https"] as? String)!)
         let data = try? Data(contentsOf: imageUrl!)
         
@@ -99,6 +102,9 @@ class homeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
